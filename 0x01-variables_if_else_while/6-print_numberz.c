249f880af41d59e642c7b9c16ddc1c44290ee3c1
hassanql <hassanminivice@gmail.com>
2023-09-08 17:48:43 -0700

    6-print_numberz done finalluy

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,22 +2,26 @@
 
 
 /**
-* main - prints all the numbers of base 16 in lowercase.
+* main- prints all possible combinations of single-digit numbers
 *
 * Return: Always 0.
 *
 */
 
-int main(void)
+	int main(void)
 {
-	int num;
-	char letter;
+		int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
+	{
 		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
+		putchar(',');
+		putchar(' ');
+
+	}
 
 	putchar('\n');
 	return (0);
