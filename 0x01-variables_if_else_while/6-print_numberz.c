1c682d6a19696fd9781171be4dc033a360409aa4
hassanql <hassanminivice@gmail.com>
2023-09-08 17:46:18 -0700

    6-print_numberz done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 
 /**
-* main - prints the lowercase alphabet in reverse, followed by a new line
+* main - prints all the numbers of base 16 in lowercase.
 *
 * Return: Always 0.
 *
@@ -10,13 +10,15 @@
 
 int main(void)
 {
+	int num;
 	char letter;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
-	putchar(letter);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 
 	putchar('\n');
-
 	return (0);
 }
