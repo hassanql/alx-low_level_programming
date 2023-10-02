6d6e7983606e190ec072375c565bc1ec1e90c8a9
hassanql <hassanminivice@gmail.com>
2023-10-02 12:34:01 -0700

    lesgoo

---
+++ 0x00-hello_world/testt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*
+* main - this is the main put function
+*
+* Return: Alawys 0 (success)
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+                return (0);
+}
